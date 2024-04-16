a1bbd0dbd17b4d6403642bb440ea877bdae73333
wanglei <lwan626@aucklanduni.ac.nz>
2024-04-16 23:00:34 +1200

    - add .env - test data display

--- KaleMeMaybe/backend/data/recipe-dao.js
+++ KaleMeMaybe/backend/data/recipe-dao.js
@@ -1,8 +1,17 @@
 const SQL = require("sql-template-strings");
 const dbPromise = require("./database.js");
 
+async function retrieveRecipeById(id){
+    const db = await dbPromise;
+
+    const recipe = await db.get(
+        `select * from recipe where id=${id}`
+    )
+
+    return recipe;
+}
 
 // Export functions.
 module.exports = {
-  
+    retrieveRecipeById
 };

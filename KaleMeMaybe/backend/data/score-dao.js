6be4f5b56e7d75c178131ad49ce70343da7d90b8
wanglei <lwan626@aucklanduni.ac.nz>
2024-04-19 11:44:24 +1200

    - update rating logic - display average score

--- KaleMeMaybe/backend/data/score-dao.js
+++ KaleMeMaybe/backend/data/score-dao.js
@@ -7,7 +7,9 @@ async function updateUserRating(userId, recipeId,score) {
     try {
         await db.run(SQL`
             INSERT INTO score (user_id, recipe_id, score)
-        VALUES (${userId}, ${recipeId}, ${score});
+        VALUES (${userId}, ${recipeId}, ${score})
+        ON CONFLICT(user_id, recipe_id) 
+        DO UPDATE SET score = excluded.score, updated_at = CURRENT_TIMESTAMP;
         `);
         return { message: "Rating updated successfully" };
     } catch (error) {
@@ -16,7 +18,30 @@ async function updateUserRating(userId, recipeId,score) {
     }
 }
 
+async function getAverageScore(recipeId) {
+    const db = await dbPromise;
+    try {
+        const result = await db.get(SQL`
+            SELECT AVG(score) AS averageScore FROM score WHERE recipe_id = ${recipeId};
+        `);
+
+        if (result && result.averageScore !== null) {
+            return { averageScore: parseFloat(result.averageScore.toFixed(1)) }; // Format to one decimal place and return
+        } else {
+            // Handle the case where no scores are found or calculation is not possible
+            return { averageScore: "0" };
+        }
+    } catch (error) {
+        console.error("Error retrieving average score:", error);
+        throw new Error(`Failed to retrieve average score: ${error.message}`);
+    }
+}
+
+
+
+
 // Export functions.
 module.exports = {
-    updateUserRating
+    updateUserRating,
+    getAverageScore
 };

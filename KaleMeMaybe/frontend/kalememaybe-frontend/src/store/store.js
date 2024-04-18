1758803fd74679c4c1d8df14f56983188bdda547
Lyvia Huang <126723293+kesesek@users.noreply.github.com>
2024-04-18 21:06:29 +1200

    update store.js

--- KaleMeMaybe/frontend/kalememaybe-frontend/src/store/store.js
+++ KaleMeMaybe/frontend/kalememaybe-frontend/src/store/store.js
@@ -1,5 +1,6 @@
 // This file defines a Zustand store for state management across 
 // the application by allowing any component to access and update data.
+// Run `npm install zustand` to install it.
 import { create } from 'zustand';
 
 const useStore = create(set => ({

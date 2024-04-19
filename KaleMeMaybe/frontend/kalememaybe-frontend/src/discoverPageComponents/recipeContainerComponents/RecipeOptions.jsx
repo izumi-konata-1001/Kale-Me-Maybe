c464309e3497407685f6d546f3633b092a1cd5eb
Jennie <yjzhu981001@qq.com>
2024-04-19 19:14:54 +1200

    sort backend

--- KaleMeMaybe/frontend/kalememaybe-frontend/src/discoverPageComponents/recipeContainerComponents/RecipeOptions.jsx
+++ KaleMeMaybe/frontend/kalememaybe-frontend/src/discoverPageComponents/recipeContainerComponents/RecipeOptions.jsx
@@ -1,15 +1,14 @@
-// RecipeOptions component
 import React from 'react';
 import SortOptions from './recipeOpationComponents/SortOptions';
 import ViewOptions from './recipeOpationComponents/ViewOptions';
 
-const RecipeOptions = ({ activeView, setActiveView }) => {
-  return (
-    <div className="flex justify-between items-center p-4">
-      <SortOptions />
-      <ViewOptions activeView={activeView} setActiveView={setActiveView} />
-    </div>
-  );
+const RecipeOptions = ({ activeView, setActiveView, onSortChange }) => {
+    return (
+        <div className="flex justify-between items-center p-4">
+            <SortOptions onSortChange={onSortChange} />
+            <ViewOptions activeView={activeView} setActiveView={setActiveView} />
+        </div>
+    );
 };
 
 export default RecipeOptions;

8dce68ec2549ae4d8029f99a0397a0640ec37bfa
unknown <zliu702@aucklanduni.ac.nz>
2024-04-22 15:37:38 +1200

    Add some useEffect()

--- KaleMeMaybe/backend/routes/histories.js
+++ KaleMeMaybe/backend/routes/histories.js
@@ -3,12 +3,30 @@ const router = express.Router();
 const broDao = require("../data/browsing-history-dao.js");
 
 router.get("/browsing", async (req, res) => {
-    //const user = req.headers["userid"];
-    //const userId = req.headers.userid;
-    const user_id = 1;
+
+     const user_id = req.body.userId;
 
     try {
+      // res.status(200).json([{
+      //   user_id : 1, 
+      //   recipe_id: 1, 
+      //   date: new Date(), 
+      //   name: "meat",
+      //   time_consuming: "25 min", 
+      //   difficulty: "easy", 
+      //   method: "balbalbal", 
+      //   image_path: "https://www.researchgate.net/publication/367813651/figure/fig2/AS:11431281116808833@1675330640498/The-Tupian-languages-with-the-sub-branches-of-the-Mawe-Aweti-Tupi-Guarani-branch.ppm"
+      // },{user_id : 2, 
+      //   recipe_id: 2, 
+      //   date: new Date(), 
+      //   name: "vegetable",
+      //   time_consuming: "12 min", 
+      //   difficulty: "hard", 
+      //   method: "balbalbal", 
+      //   image_path: "https://www.researchgate.net/publication/367813651/figure/fig2/AS:11431281116808833@1675330640498/The-Tupian-languages-with-the-sub-branches-of-the-Mawe-Aweti-Tupi-Guarani-branch.ppm"}]);
+
         const history = await broDao.getBrowsingHistory(user_id);
+
         if (history) {
           res.status(200).json(history);
           console.log(history);

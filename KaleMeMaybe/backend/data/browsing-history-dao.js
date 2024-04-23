dcedaac47f4162e046fed9a1d7c64d835577703f
unknown <zliu702@aucklanduni.ac.nz>
2024-04-23 12:36:59 +1200

    finished all browsing history logic

--- KaleMeMaybe/backend/data/browsing-history-dao.js
+++ KaleMeMaybe/backend/data/browsing-history-dao.js
@@ -5,18 +5,50 @@ async function getBrowsingHistory(userId) {
     const db = await dbPromise;
     try {
         const history = await db.all(SQL`
-        SELECT b.user_id, b.recipe_id, b.created_at AS date, r.name, r.time_consuming, r.difficulty, r.method, r.image_path
+        SELECT DISTINCT b.user_id, b.recipe_id, r.name, r.time_consuming, r.difficulty, r.method, r.image_path
         FROM browsing_history b JOIN recipe r ON b.recipe_id = r.id 
-        WHERE b.user_id = ${userId}`);
+        WHERE b.user_id = ${userId}
+        ORDER BY b.created_at DESC
+        `);
         return history;
     } catch (err) {
         console.error('Failed to retrieve browsing histories from the database:', err);
         throw err;
     }
-  }
+}
+
+async function updateBrowsingHistory(userId, recipeId) {
+    const db = await dbPromise;
+    try {
+        await db.run(SQL`
+        INSERT OR REPLACE INTO browsing_history (user_id, recipe_id, created_at)
+        VALUES (${userId}, ${recipeId}, CURRENT_TIMESTAMP);`);
+    } catch (err) {
+        console.error('Failed to insert browsing histories to the database:', err);
+        throw err;
+    }
+}
+
+async function getRecipesWithIds(recipeIds) {
+    const db = await dbPromise; 
+    try {
+        const placeholders = recipeIds.map(() => '?').join(', '); 
+        const query = `
+        SELECT id AS recipe_id, name, time_consuming, difficulty, method, image_path
+        FROM recipe WHERE id IN (${placeholders}) 
+        ORDER BY created_at DESC`;
+        const recipes = await db.all(query, recipeIds); 
+        return recipes;
+    } catch (err) {
+        console.error('Failed to retrieve local browsing recipes:', err);
+        throw err;
+    }
+}
 
 
 // Export functions.
 module.exports = {
-    getBrowsingHistory
+    getBrowsingHistory,
+    updateBrowsingHistory,
+    getRecipesWithIds
 };

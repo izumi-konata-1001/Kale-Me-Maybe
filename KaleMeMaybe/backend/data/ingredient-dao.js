92d7f6cc00df62c2290b27d29719e9ecd46bc4b1
Yutian1024-nz~ <147520483@qq.com>
2024-04-24 15:56:20 +1200

    ingredient suggestion SQL updated

--- KaleMeMaybe/backend/data/ingredient-dao.js
+++ KaleMeMaybe/backend/data/ingredient-dao.js
@@ -3,13 +3,12 @@ const dbPromise = require("./database.js");
 
 async function queryIngredients(prefix) {
   const db = await dbPromise;
-  const query = SQL`SELECT id, name FROM ingredient WHERE name LIKE ${
-    prefix + "%"
+  const query = SQL`SELECT id, name FROM ingredient WHERE LOWER(name) LIKE ${
+    "%" + prefix.toLowerCase() + "%"
   } ORDER BY name`;
   const ingredients = await db.all(query);
   return ingredients;
 }
-
 
 // Export functions.
 module.exports = {

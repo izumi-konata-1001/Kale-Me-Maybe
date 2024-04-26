bff12b44ebba797f6ef811a69b2262daeef24961
Lyvia Huang <126723293+kesesek@users.noreply.github.com>
2024-04-26 20:42:02 +1200

    edit

--- KaleMeMaybe/frontend/kalememaybe-frontend/src/discoverPageComponents/recipeContainerComponents/RecipeGrid.jsx
+++ KaleMeMaybe/frontend/kalememaybe-frontend/src/discoverPageComponents/recipeContainerComponents/RecipeGrid.jsx
@@ -3,8 +3,8 @@ import RecipeCard from "./recipeGridComponents/RecipeCard";
 const RecipeGrid = ({ recipes, onToggleSelect, isSelected, showCheckboxes }) => {
   return (
     <div className="grid grid-cols-1 gap-8 s:grid-cols-2 m:grid-cols-3">
-      {recipes.map((recipe, index) => (
-        <RecipeCard key={index} recipe={recipe} onToggleSelect={onToggleSelect} isSelected={isSelected} showCheckboxes={showCheckboxes} />
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.id} recipe={recipe} onToggleSelect={onToggleSelect} isSelected={isSelected} showCheckboxes={showCheckboxes} />
       ))}
     </div>
   );

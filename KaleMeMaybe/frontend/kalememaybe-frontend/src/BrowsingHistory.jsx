4d86f3b782372a8900410db1d5382c92756eb4ac
unknown <zliu702@aucklanduni.ac.nz>
2024-04-29 18:57:18 +1200

    fix responsive layout and collections clickable and remove the scrollbar

--- KaleMeMaybe/frontend/kalememaybe-frontend/src/BrowsingHistory.jsx
+++ KaleMeMaybe/frontend/kalememaybe-frontend/src/BrowsingHistory.jsx
@@ -49,19 +49,20 @@ export default function BrowsingHistory() {
   }, [authToken]);
 
   return (
+    <>
     <div className="flex flex-col items-center BrowsingHistory">
       <h1 className="title">Browsing History</h1>
-      <div className="mt-10 mb-10 search-div">
+      <div className="mt-10 mb-10 flex w-full md:w-3/4 search-div">
         <input
           type="text"
           name="search"
-          className="border border-gray-300 border-r-0 h-10 bg-gray-100 rounded-tl-md rounded-bl-md search"
+          className="flex-grow border border-gray-300 border-r-0 h-10 bg-gray-100 rounded-tl-md rounded-bl-md search"
           placeholder="  Search for your specify recipes..."
-          style={{ width: "600px" }}
+        //   style={{ width: "600px" }}
           onChange={(e) => setSearchValue(e.target.value)}
         />
         <button
-          className="mt-4 bg-green-dark text-white px-4 py-2 rounded-md rounded-tl-none rounded-bl-none btt"
+          className="bg-green-dark text-white px-4 py-2 rounded-md rounded-tl-none rounded-bl-none btt"
           onClick={() => {
             setFilter(searchValue);
             console.log(filterValue);
@@ -72,7 +73,7 @@ export default function BrowsingHistory() {
       </div>
       {broData.length ? (
         <div
-          className="flex flex-col items-center overflow-y-auto cardset"
+          className="flex flex-col items-center overflow-y-auto hide-scrollbar cardset"
           style={{ maxHeight: "500px" }}
         >
           {broData
@@ -81,32 +82,41 @@ export default function BrowsingHistory() {
             )
             .map((item) => (
               <div
-                className="relative border border-gray-200 w-2/3 p-3 px-4 mb-6 shadow card"
+                className="relative border border-gray-200 w-full md:w-2/3 p-3 px-4 mb-6 shadow card"
+                // key={item.recipe_id}
+                // onClick={() => navigate(`/recipe/${item.recipe_id}`)}
+              >
+                <div className="absolute w-44 h-44 border-2 border-green-dark rounded-lg mr-12 mt-5 mb-5 ml-5 overflow-hidden picture"
                 key={item.recipe_id}
                 onClick={() => navigate(`/recipe/${item.recipe_id}`)}
-              >
-                <div className="absolute w-44 h-44 border-2 border-green-dark rounded-lg mr-12 mt-5 mb-5 ml-5 overflow-hidden picture">
+                >
                   <img src={`${API_BASE_URL}/${item.image_path}`}></img>
                 </div>
                 <div className="ml-60 relative content">
-                  <h3 className="text-xl font-mono font-bold pt-1 pb-1 mt-5 mb-1.5 subtitle">
+                  <h3 className="text-xl font-mono font-bold pt-1 pb-1 mt-5 mb-1.5 subtitle"
+                  key={item.recipe_id}
+                  onClick={() => navigate(`/recipe/${item.recipe_id}`)}
+                  >
                     {item.name}
                   </h3>
-                  <div className="flex flex-row mt-3 mb-3 ml-0 mr-0 fouricons">
-                    <p className="rounded-xl px-3 py-1 text-sm font-semibold bg-green-light text-green-dark shadow mr-5 h-7.5 firsttwo">
+                  <div className="flex flex-row flex-wrap mt-3 mb-3 ml-0 mr-0 gap-2 fouricons">
+                    <p className="rounded-xl px-3 py-1 text-sm font-semibold bg-green-light text-green-dark shadow mr-5 h-7.5 mb-2 firsttwo">
                       {item.time_consuming}
                     </p>
-                    <p className="rounded-xl px-3 py-1 text-sm font-semibold bg-green-light text-green-dark shadow mr-5 h-7.5 firsttwo">
+                    <p className="rounded-xl px-3 py-1 text-sm font-semibold bg-green-light text-green-dark shadow mr-5 h-7.5 mb-2 firsttwo">
                       {item.difficulty}
                     </p>
-                    <div className="mr-5 h-2.5">
+                    <div className="mr-5 h-2.5 mb-2">
                       <RecipeScoreIcon recipeId={item.recipe_id} />
                     </div>
-                    <div className="favIcon -mt-3.5 mr-5 h-2.5">
+                    <div className="favIcon -mt-3.5 mr-5 h-2.5 mb-2">
                       {authToken && <RecipeFavouriteIcon />}
                     </div>
                   </div>
-                  <p className="text-base leading-normal text-gray-600 break-words w-87 h-24 overflow-hidden text-ellipsis des">
+                  <p className="text-base leading-normal text-gray-600 break-words w-87 h-24 overflow-hidden text-ellipsis des"
+                  key={item.recipe_id}
+                  onClick={() => navigate(`/recipe/${item.recipe_id}`)}
+                  >
                     {item.method}
                   </p>
                 </div>
@@ -117,5 +127,15 @@ export default function BrowsingHistory() {
         <p>No browsing history ever</p>
       )}
     </div>
+    <style jsx>{`
+        .hide-scrollbar::-webkit-scrollbar {
+          display: none;
+        }
+        .hide-scrollbar {
+          -ms-overflow-style: none;
+          scrollbar-width: none;
+        }
+      `}</style>
+    </>
   );
 }

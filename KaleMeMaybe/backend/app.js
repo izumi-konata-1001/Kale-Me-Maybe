d22fa7a288306d2da67b23b0138af16818d88fa1
xjia357 <xjia357@aucklanduni.ac.nz>
2024-05-01 14:35:35 +1200

    change port to allow eb and user-dao to mysql

--- KaleMeMaybe/backend/app.js
+++ KaleMeMaybe/backend/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Set up cross-domain access
 const cors = require("cors");

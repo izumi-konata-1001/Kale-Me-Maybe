4b85d22aae42d460686d4951e92e87b53e05d5ec
Jennie <yjzhu981001@qq.com>
2024-05-06 17:32:15 +1200

    fix size bug

--- KaleMeMaybe/frontend/kalememaybe-frontend/src/discoverPageComponents/recipeContainerComponents/recipeListComponents/RecipeCard.jsx
+++ KaleMeMaybe/frontend/kalememaybe-frontend/src/discoverPageComponents/recipeContainerComponents/recipeListComponents/RecipeCard.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const RecipeCard = ({ recipe }) => {
   return (
-    <div className="flex flex-col items-center w-full mx-auto p-4 border border-grey rounded-lg shadow-md list-s:flex-row ">
-      <div className="flex justify-center flex-initial pt-2 list-l:w-2/6 list-m:w-1/2">
+    <div className="flex flex-col w-full p-3 border border-grey rounded-lg shadow-md list-s:flex-row list-s:justify-center list-xs:justify-center">
+      <div className="flex justify-center flex-initial pt-2 list-l:w-2/6 list-m:w-1/2 list-m:pl-2">
         <Link to={`/recipe/${recipe.id}`}>
           <RecipeImageWithBorder recipe={recipe} />
         </Link>
       </div>
-      <div className="flex-initial pb-2 list-l: w-4/6 pl-5 list-m: w-1/2">
+      <div className="w-full flex-initial pb-2 list-l: w-4/6 list-l:pl-5 list-m:pl-5 list-s:pl-5">
         <RecipeDetail recipe={recipe} />
       </div>
     </div>

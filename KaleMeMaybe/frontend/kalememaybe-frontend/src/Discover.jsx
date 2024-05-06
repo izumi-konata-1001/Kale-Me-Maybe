8a126aafad6d80bf425c2a82a05b9dee36980e52
Jennie <yjzhu981001@qq.com>
2024-05-06 16:19:11 +1200

    change title

--- KaleMeMaybe/frontend/kalememaybe-frontend/src/Discover.jsx
+++ KaleMeMaybe/frontend/kalememaybe-frontend/src/Discover.jsx
@@ -54,7 +54,7 @@ const Discover = () => {
   return (
     <div className="recipes-scrollable max-h-screen overflow-y-auto w-full">
       <div className="flex flex-col items-center justify-center pb-2">
-        <h1 className="title">Pantry</h1>
+        <h1 className="title">Veggie Voyage</h1>
         <h3 className="text-1xl font-mono pt-2 pb-3 dark:text-white">
           Explore the Green Gastronomy
         </h3>
